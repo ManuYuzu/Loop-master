9d9f1794beab3d7e9be18d54032dd49e5362fb6d
Alexis <silver.mugiwara@gmail.com>
2021-05-24 14:34:14 +0100

    enemy created

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let LEVELS = {
+const LEVELS = {
   // 0 = Vacio / 1 = Camino / 2 = Character / 3 = Enemigos / 4 = Moneda
   L1: [
   // 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17
@@ -11,7 +11,7 @@ let LEVELS = {
     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,11 +22,13 @@ let LEVELS = {
   ]
 }
 
-let Character = function () {
+const Character = function () {
   this.y = 5
   this.x = 8
   this.previousY = 0
   this.previousX = 0
+  this.strength = 5
+  this.health = 20
 
   this.StartPosition = function () {
     LEVELS.L1[this.y][this.x] = 2
@@ -50,20 +52,37 @@ let Character = function () {
       this.previousY = this.y
       this.previousX = this.x
       this.y--
+    } else if (LEVELS.L1[this.y][this.x + 1] === 3 && this.x + 1 !== this.previousX) {
+      combat()
+    } else if (LEVELS.L1[this.y + 1][this.x] === 3 && this.y + 1 !== this.previousY) {
+      combat()
+    } else if (LEVELS.L1[this.y][this.x - 1] === 3 && this.x - 1 !== this.previousX) {
+      combat()
+    } else if (LEVELS.L1[this.y - 1][this.x] === 3 && this.y - 1 !== this.previousY) {
+      combat()
     }
     LEVELS.L1[this.y][this.x] = 2
-    console.log(this.previousY, this.previousX)
-    console.log(this.y, this.x)
   }
 }
 
-let char = new Character()
+const EnemyMaker = function () {
+  this.y = 0
+  this.x = 0
+  this.strength = 1
+  this.health = 5
+}
+
+const combat = function () {
+  clearInterval(gameTimer)
+  console.log('pelea')
+}
+const char = new Character()
 
 function drawBoard () {
   LEVELS.L1.forEach((row, r) => {
     row.forEach((col, c) => {
-      let rowHTML = document.querySelector(`.row${r + 1}`)
-      let cellHTML = rowHTML.querySelector(`.col${c + 1}`)
+      const rowHTML = document.querySelector(`.row${r + 1}`)
+      const cellHTML = rowHTML.querySelector(`.col${c + 1}`)
 
       if (LEVELS.L1[r][c] === 1) {
         cellHTML.classList.remove('character')
@@ -74,6 +93,11 @@ function drawBoard () {
         cellHTML.classList.remove('road')
         cellHTML.classList.add('character')
       }
+
+      if (LEVELS.L1[r][c] === 3) {
+        cellHTML.classList.remove('road')
+        cellHTML.classList.add('enemy')
+      }
     })
   })
 }
@@ -83,7 +107,6 @@ function game () {
   char.moveCharacter()
 }
 
-let gameTimer = setInterval(game, 300)
-
+// ejecucion
 char.StartPosition()
-game()
+const gameTimer = setInterval(game, 300)
